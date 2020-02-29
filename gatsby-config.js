13e98584a57191444fd0c2d3c2da7782283a3d8a
Leonardo Sguotti <39900798+leo-sgruz@users.noreply.github.com>
2020-02-29 10:13:02 +0100

    Fix configuration syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ var configuration = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
